a34586bb87f3b518f797d56cc1e894b8fcbb5d19
Tim-odm <t.makoni.tm16@gmail.com>
2024-07-03 18:00:34 +0200

    Add: sidebar options icons

--- src/scripts/sidebar.js
+++ src/scripts/sidebar.js
@@ -1,3 +1,9 @@
+// Icon imports
+import calIcon from "../assets/icons/calendar-today-outline.svg";
+import inboxIcon from "../assets/icons/inbox.svg";
+import projectsIcon from "../assets/icons/hammer-wrench.svg";
+
+// Draw sidebar DOM elements
 export function drawSideBar() {
   const sidebarDiv = document.createElement("div");
   sidebarDiv.classList.add("sidebar");
@@ -5,15 +11,18 @@ export function drawSideBar() {
   const sidebarOptions = document.createElement("ul");
   sidebarOptions.classList.add("sidebar-options");
   const inboxOption = document.createElement("li");
-  inboxOption.textContent = "Inbox";
+  inboxOption.innerHTML = `${inboxIcon} <p>Inbox</p>`;
   const todayOption = document.createElement("li");
-  todayOption.textContent = "Today";
+  todayOption.innerHTML = `${calIcon} <p>Today</p>`;
   const projectsOption = document.createElement("li");
-  projectsOption.textContent = "Projects";
+  projectsOption.innerHTML = `${projectsIcon} <p>Projects</p>`;
   const projectListDiv = document.createElement("div");
   sidebarOptions.append(inboxOption, todayOption, projectsOption, projectListDiv);
-  sidebarDiv.appendChild(sidebarOptions);
+  for (let i = 0; i < sidebarOptions.childElementCount - 1; i++) {
+    sidebarOptions.children[i].children[0].classList.add("icon");
+  }
 
+  sidebarDiv.appendChild(sidebarOptions);
   document.querySelector("#app").appendChild(sidebarDiv);
 }
 
