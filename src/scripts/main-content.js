b3b94edd5b7c3f402bc1c423dd0de20841e66415
Tim-odm <t.makoni.tm16@gmail.com>
2024-07-16 13:02:54 +0200

    add: updateTodoListDiv

--- src/scripts/main-content.js
+++ src/scripts/main-content.js
@@ -1,4 +1,5 @@
-
+// Module imports
+import { currentProject } from "./todo-manager";
 
 function drawMainContent() {
   const mainDiv = document.createElement("div");
@@ -9,7 +10,35 @@ function drawMainContent() {
   projectHeader.innerText = "Project Header";
   mainDiv.appendChild(projectHeader);
 
+  const todoListDiv = updateTodoListDiv();
+  mainDiv.appendChild(todoListDiv);
+
   document.querySelector("#app").appendChild(mainDiv);
+}
+
+export function updateTodoListDiv() {
+  const todoListDiv = document.createElement("div");
+  todoListDiv.classList.add("todo-list-div");
+
+  currentProject.todos.forEach(todo => {
+    console.log(todo);
+    const todoItem = document.createElement("div");
+    todoItem.classList.add("todo-item", todo.priority);
+
+    const todoTitle = document.createElement("h4");
+    todoTitle.innerText = todo.title;
+
+    const todoDesc = document.createElement("p");
+    todoDesc.innerText = todo.description;
+
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+
+    todoItem.append(todoTitle, todoDesc, checkbox);
+    todoListDiv.appendChild(todoItem);
+  });
+
+  return todoListDiv;
 }
 
 drawMainContent()
