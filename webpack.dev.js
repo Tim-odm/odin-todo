e9d3cffc34adf396c51d12e5ca78e25995818d82
Tim-odm <t.makoni.tm16@gmail.com>
2024-08-26 17:51:38 +0200

    common dev and prod configs

---
+++ webpack.dev.js
@@ -0,0 +1,34 @@
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
+
+module.exports = merge(common, {
+  mode: "development",
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.svg$/i,
+        type: "asset/source",
+      },
+      {
+        test: /favicon.svg$/i,
+        type: "asset/inline",
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
+});
