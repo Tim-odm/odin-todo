820fb14f69f2dfc3ccdbce31932c5f7f1291d97f
Tim-odm <t.makoni.tm16@gmail.com>
2024-08-26 18:07:22 +0200

    add loaders to common config file

--- webpack.common.js
+++ webpack.common.js
@@ -5,6 +5,30 @@ module.exports = {
   entry: {
     app: "./src/scripts/index.js",
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      },
+      {
+        test: /\.svg$/i,
+        type: "asset/source",
+      },
+      {
+        test: /favicon.svg$/i,
+        type: "asset/inline"
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/i,
+        type: "asset/resource",
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: "Production",
