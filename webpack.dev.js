528c34c47965c5224d59fafdf0d870644de2a23b
Tim-odm <t.makoni.tm16@gmail.com>
2024-08-26 18:08:02 +0200

    remove loaders from dev config

--- webpack.dev.js
+++ webpack.dev.js
@@ -7,28 +7,4 @@ module.exports = merge(common, {
   devServer: {
     static: "./dist",
   },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.svg$/i,
-        type: "asset/source",
-      },
-      {
-        test: /favicon.svg$/i,
-        type: "asset/inline",
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-      },
-    ],
-  },
 });
